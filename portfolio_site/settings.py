3771c3edf4a084047f706b8775cb00948ce260de
Israel <israel.klein@jjay.cuny.edu>
2022-01-11 18:13:42 -0500

    .

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'quote_generator',
+    'blog',
+    'portfolio',
+    'portfolio_site',
 ]
 
 MIDDLEWARE = [
@@ -105,9 +109,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STATIC_DIRS = [
+ #   BASE_DIR / 'quote_generator' / 'static',
+ #   BASE_DIR / 'movie_generator' / 'static',
+]
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- quote_generator/views.py
+++ quote_generator/views.py
@@ -3,8 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
+def index(request):
     return render(request, 'quote_generator/index.html')
-
-def home(request):
-    return render(request, 'quote_generator/about.html')
